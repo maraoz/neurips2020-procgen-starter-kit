1bde0af3e251d446497a15f06c48a96ecba665ed
Manuel Araoz <manuelaraoz@gmail.com>
2020-07-11 17:56:46 -0300

    v0.5 i guess

--- send.sh
+++ send.sh
@@ -10,7 +10,8 @@ git tag -am "submission-v0.$version" submission-v0.$version
 git push aicrowd master
 git push aicrowd submission-v0.$version
 
+echo $version > version.txt
+
 git add version.txt
 git commit -m"bump version to v0.$version"
 
-echo $version > version.txt
