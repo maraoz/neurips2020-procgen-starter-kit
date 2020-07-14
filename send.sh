7fefb91b6eebd4bbdaa539c4a40f3a4dfccd1cde
Manuel Araoz <manuelaraoz@gmail.com>
2020-07-14 20:12:02 -0300

    even gentler on memory

--- send.sh
+++ send.sh
@@ -6,12 +6,14 @@ echo "previous version $version"
 version=`expr $version + 1`
 echo "current version $version"
 
-git tag -am "submission-v0.$version" submission-v0.$version
+echo "version name: $version-$1"
+
+git tag -am "submission-v0.$version-$1" submission-v0.$version-$1
 git push aicrowd master
-git push aicrowd submission-v0.$version
+git push aicrowd submission-v0.$version-$1
 
 echo $version > version.txt
 
 git add version.txt
-git commit -m"bump version to v0.$version"
+git commit -m"bump version to v0.$version-$1"
 
