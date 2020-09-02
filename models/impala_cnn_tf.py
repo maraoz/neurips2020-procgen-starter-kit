17ef1cc530d989c84c323b21141c734180a3c089
Manuel Araoz <manuelaraoz@gmail.com>
2020-09-02 20:31:46 -0300

    revert to sample code

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -24,9 +24,9 @@ def residual_block(x, spec, prefix):
     #first['kernel'] = 1
     #first['strides'] = 1
     x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Dense(units=first['depth']**2, activation="relu", name=prefix + "_dense")(x)
+    x = conv_layer(first, name=prefix + "_conv0")(x)
+    x = tf.keras.layers.ReLU()(x)
     x = conv_layer(spec, name=prefix + "_conv1")(x)
-    x = tf.keras.layers.BatchNormalization()(x)
     return x + inputs
     x = tf.keras.layers.ReLU()(x)
     x = conv_layer(first, name=prefix + "_conv2")(x)
@@ -47,9 +47,8 @@ def conv_core(x):
 
     specs = [
         {"depth": 16, "kernel": 3, "strides": 1},
-        {"depth": 16, "kernel": 3, "strides": 1},
-        {"depth": 16, "kernel": 3, "strides": 1},
-        {"depth": 16, "kernel": 3, "strides": 1},
+        {"depth": 32, "kernel": 3, "strides": 1},
+        {"depth": 32, "kernel": 3, "strides": 1},
     ]
     for i, spec in enumerate(specs):
         x = conv_sequence(x, spec, prefix=f"seq{i}")
