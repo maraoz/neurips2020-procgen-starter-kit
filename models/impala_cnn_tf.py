1024e09e43639d4754cfc220c82352d60f509be4
Manuel Araoz <manuelaraoz@gmail.com>
2020-09-02 20:08:18 -0300

    4x16

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -47,8 +47,9 @@ def conv_core(x):
 
     specs = [
         {"depth": 16, "kernel": 3, "strides": 1},
-        {"depth": 32, "kernel": 3, "strides": 1},
-        {"depth": 32, "kernel": 3, "strides": 1},
+        {"depth": 16, "kernel": 3, "strides": 1},
+        {"depth": 16, "kernel": 3, "strides": 1},
+        {"depth": 16, "kernel": 3, "strides": 1},
     ]
     for i, spec in enumerate(specs):
         x = conv_sequence(x, spec, prefix=f"seq{i}")
